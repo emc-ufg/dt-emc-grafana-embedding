93e138be1912c9f7dbce3023a5db9cff010bdef8
antonio-emilio <antoniopereira@discente.ufg.br>
2023-12-29 09:45:05 -0300

    f:

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 const GrafanaEmbed = () => {
   return (
     <div>
+      <Head>
+        {/* Adiciona a tag de Content-Security-Policy */}
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <iframe
         src="http://200.137.220.250:3000/public-dashboards/0cc223ae37164ef4ac570a67830d2dfe?orgId=1"
         width="100%"
