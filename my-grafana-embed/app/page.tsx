086ce8d906dde5397661593bafd34d5cc05c330d
antonio-emilio <antoniopereira@discente.ufg.br>
2023-12-29 09:28:58 -0300

    f:

---
+++ my-grafana-embed/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const GrafanaEmbed = () => {
+  return (
+    <div>
+      <iframe
+        src="http://200.137.220.250:3000/public-dashboards/0cc223ae37164ef4ac570a67830d2dfe?orgId=1"
+        width="100%"
+        height="1080px"
+        frameBorder="0"
+      />
+    </div>
+  );
+};
+
+export default GrafanaEmbed;
